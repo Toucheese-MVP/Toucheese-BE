81fbca71a02fa221cd25e0e7a6811f34c3b4f856
ppusda <ppusda1234@gmail.com>
2024-11-11 15:20:36 +0900

    init: 프로젝트 생성

---
+++ toucheese/src/main/java/com/toucheese/ToucheeseApplication.java
@@ -0,0 +1,13 @@
+package com.toucheese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToucheeseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToucheeseApplication.class, args);
+	}
+
+}
---
+++ toucheese/src/test/java/com/toucheese/ToucheeseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toucheese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToucheeseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
