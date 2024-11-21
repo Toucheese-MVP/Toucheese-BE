8af9f58b83bd71223edd85010d4773dd4a218311
ppusda <51808344+ppusda@users.noreply.github.com>
2024-11-22 02:03:58 +0900

    hotfix: StudioController requestBody => queryParam으로 변경 (#14)

--- src/main/java/com/toucheese/studio/controller/StudioController.java
+++ src/main/java/com/toucheese/studio/controller/StudioController.java
@@ -1,15 +1,14 @@
 package com.toucheese.studio.controller;
 
-import com.toucheese.studio.dto.FilteringStudioRequest;
 import com.toucheese.studio.dto.StudioResponse;
+import com.toucheese.studio.dto.StudioSearchResponse;
+import com.toucheese.studio.entity.Location;
+import com.toucheese.studio.service.StudioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-import com.toucheese.studio.dto.StudioSearchResponse;
-import com.toucheese.studio.service.StudioService;
 
 @RestController
 @RequestMapping("/api/v1/studios")
@@ -26,13 +25,22 @@ public class StudioController {
 	/**
 	 * 필터링 한 스튜디오 목록을 조회한다.
 	 * @param page 현재 페이지
-	 * @param request 필터링 조건 (가격, 인기, 지역)
+	 * @param conceptId 선택된 컨셉 요소 아이디
+	 * @param price 가격순 필터링 요소
+	 * @param rating 인기순 필터링 요소
+	 * @param locations 지역 필터링 요소
 	 * @return 현재 페이지에 해당하는 필터링 된 스튜디오 목록
 	 */
-	@GetMapping("/filters")
-	public Page<StudioResponse> getFilteredStudios(@RequestParam int page, @RequestBody FilteringStudioRequest request) {
+	@GetMapping("/{conceptId}/filters")
+	public Page<StudioResponse> getFilteredStudios(
+			@PathVariable Long conceptId,
+			@RequestParam int page,
+			@RequestParam(required = false) Integer price,
+			@RequestParam(required = false) Float rating,
+			@RequestParam(required = false) List<Location> locations
+	) {
 		return studioService.getFilteredStudiosOrderByName(
-				page, request.conceptId(), request.price(), request.rating(), request.locations()
+				page, conceptId, price, rating, locations
 		);
 	}
 
