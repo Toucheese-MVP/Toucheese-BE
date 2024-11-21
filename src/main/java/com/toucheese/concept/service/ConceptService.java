806b21337079d2fa66f62e99eda0ceb9103c78be
정두콩 <junggg097@daum.net>
2024-11-21 14:41:18 +0900

    feat : 컨셉 전체 조회 기능 구현 (#12)

--- src/main/java/com/toucheese/concept/service/ConceptService.java
+++ src/main/java/com/toucheese/concept/service/ConceptService.java
@@ -1,11 +1,13 @@
 package com.toucheese.concept.service;
 
+import com.toucheese.concept.dto.ConceptDTO;
 import com.toucheese.concept.entity.Concept;
 import com.toucheese.concept.repository.ConceptRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,4 +15,10 @@ public class ConceptService {
 
     private final ConceptRepository conceptRepository;
 
+    public List<ConceptDTO> getAllConcepts() {
+        List<Concept> concepts = conceptRepository.findAll();
+        return concepts.stream()
+                .map(concept -> new ConceptDTO(concept.getId(), concept.getName()))
+                .collect(Collectors.toList());
+    }
 }
