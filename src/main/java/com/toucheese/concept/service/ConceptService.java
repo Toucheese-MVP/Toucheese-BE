517aa414baf2d04b7dbb50d1fc4e5eb0206ce284
ppusda <51808344+ppusda@users.noreply.github.com>
2024-11-29 14:44:59 +0900

    [공통] 2차 스프린트 코드 리팩토링 (#35)
    * refactor: Transactional 어노테이션 추가
    
    * refactor: CustomException 적용
    
    * refactor: Collectors.toList() => toList() 로 변경 (불변 리스트)
    
    * refactor: DTO => Response 네이밍 변경
    
    * chore: gitignore 수정
    
    * remove: 불필요한 커밋파일 삭제 (.idea, QueryDSL generated)

--- src/main/java/com/toucheese/concept/service/ConceptService.java
+++ src/main/java/com/toucheese/concept/service/ConceptService.java
@@ -1,10 +1,11 @@
 package com.toucheese.concept.service;
 
-import com.toucheese.concept.dto.ConceptDTO;
+import com.toucheese.concept.dto.ConceptResponse;
 import com.toucheese.concept.entity.Concept;
 import com.toucheese.concept.repository.ConceptRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,10 +16,11 @@ public class ConceptService {
 
     private final ConceptRepository conceptRepository;
 
-    public List<ConceptDTO> getAllConcepts() {
+    @Transactional(readOnly = true)
+    public List<ConceptResponse> getAllConcepts() {
         List<Concept> concepts = conceptRepository.findAll();
         return concepts.stream()
-                .map(ConceptDTO::of)
-                .collect(Collectors.toList());
+                .map(ConceptResponse::of)
+                .toList();
     }
 }
