68958048bfad9fed6b9537b37e64ea5669751b11
전화철 <lunaris0206@naver.com>
2024-12-03 15:27:15 +0900

    feat : 예약 기능 연관관계 설정
    ProductAddOption Entity - id타입 Long으로 변경

---
+++ src/main/java/com/toucheese/reservation/entity/ReservationProductAddOption.java
@@ -0,0 +1,33 @@
+package com.toucheese.reservation.entity;
+
+import com.toucheese.product.entity.ProductAddOption;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReservationProductAddOption {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private Integer addPrice;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_add_option_id", nullable = false)
+	private ProductAddOption productAddOption;
+
+	public ReservationProductAddOption(ProductAddOption productAddOption, Integer addPrice) {
+		this.productAddOption = productAddOption;
+		this.addPrice = addPrice;
+	}
+}
