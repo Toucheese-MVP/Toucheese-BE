bbf1f0a86efcc70dcfed3dad492a3391781decc3
전화철 <lunaris0206@naver.com>
2024-12-03 15:28:17 +0900

    feat : 예약 정보 저장 기능을 위한 repository 생성

---
+++ src/main/java/com/toucheese/product/repository/ProductAddOptionRepository.java
@@ -0,0 +1,8 @@
+package com.toucheese.product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.toucheese.product.entity.ProductAddOption;
+
+public interface ProductAddOptionRepository extends JpaRepository<ProductAddOption, Long> {
+}
