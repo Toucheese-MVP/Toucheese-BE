e3f6e79be6be4854ef4a38a50b680e69e8a07afe
전화철 <lunaris0206@naver.com>
2024-12-03 15:46:42 +0900

    feat : Swagger 예약 정보 저장 API 작성

--- src/main/java/com/toucheese/reservation/controller/ReservationController.java
+++ src/main/java/com/toucheese/reservation/controller/ReservationController.java
@@ -16,16 +16,31 @@ import com.toucheese.reservation.dto.ReservationRequest;
 import com.toucheese.reservation.entity.Reservation;
 import com.toucheese.reservation.service.ReservationService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/v1/reservations")
 @RequiredArgsConstructor
+@Tag(name = "예약 API", description = "기능 : 예약 정보 저장")
 public class ReservationController {
 	private final ReservationService reservationService;
 	private final ProductRepository productRepository;
 
 	@PostMapping("")
+	@Operation(
+		summary = "예약 정보 저장 기능",
+		description = "필요한 예약 정보 : <br>" +
+			"productId = 상품 id, <br>" +
+			"studioId = 스튜디오 id, <br>" +
+			"totalPrice = 상품 + 추가상품의 총금액, <br>" +
+			"name = 사용자 이름, <br>" +
+			"phone = 사용자 전화번호, <br>" +
+			"createDate = 예약날짜, <br>" +
+			"personnel = 예약 인원수, <br>" +
+			"addOptions = 추가 상품에서 id는 productAddOptionId 필요"
+	)
 	public ResponseEntity<ReservationRequest> reservationCreate(@RequestBody ReservationRequest reservationRequest) {
 		String name = reservationRequest.name();
 		String phone = reservationRequest.phone();
