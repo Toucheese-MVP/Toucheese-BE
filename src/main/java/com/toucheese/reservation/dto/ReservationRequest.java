03f484475d67b707f1aa642b6e0f96627a08c585
JeUk Park <77144006+sleo113@users.noreply.github.com>
2025-01-22 12:49:59 +0900

    fix: cartId 없이 예약하기 Api 관련 예시 RequestBody 수정

--- src/main/java/com/toucheese/reservation/dto/ReservationRequest.java
+++ src/main/java/com/toucheese/reservation/dto/ReservationRequest.java
@@ -15,31 +15,38 @@ import lombok.Builder;
 
 @Builder
 public record ReservationRequest(
-	Long productId,
-	Long studioId,
-	Long memberId,
-	Integer totalPrice,
-	LocalDate createDate,
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
-	@Schema(type = "string", example = "19:00")
-	LocalTime createTime,
-	Integer personnel,
-	List<Long> addOptions
+        @Schema(description = "상품 ID", example = "1")
+		Long productId,
+		@Schema(description = "스튜디오 ID", example = "1")
+        Long studioId,
+		@Schema(description = "회원 ID", example = "1")
+        Long memberId,
+		@Schema(description = "총 가격", example = "100000")
+        Integer totalPrice,
+		@Schema(description = "예약 날짜", example = "2025-01-21")
+        LocalDate createDate,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+        @Schema(type = "string", example = "19:00")
+        LocalTime createTime,
+		@Schema(description = "인원 수", example = "2")
+        Integer personnel,
+        @Schema(description = "추가 옵션 ID 목록", example = "[1, 2]")
+        List<Long> addOptions
 ) {
 
-	public static ReservationRequest of(Reservation reservation) {
-		return ReservationRequest.builder()
-			.productId(reservation.getProduct().getId())
-			.studioId(reservation.getStudio().getId())
-			.memberId(reservation.getMember().getId())
-			.totalPrice(reservation.getTotalPrice())
-			.createDate(reservation.getCreateDate())
-			.createTime(reservation.getCreateTime())
-			.personnel(reservation.getPersonnel())
-			.addOptions(reservation.getReservationProductAddOptions() != null ?
-				reservation.getReservationProductAddOptions().stream()
-					.map(ReservationProductAddOption::getId)
-					.collect(Collectors.toList()) : Collections.emptyList())
-			.build();
-	}
+    public static ReservationRequest of(Reservation reservation) {
+        return ReservationRequest.builder()
+                .productId(reservation.getProduct().getId())
+                .studioId(reservation.getStudio().getId())
+                .memberId(reservation.getMember().getId())
+                .totalPrice(reservation.getTotalPrice())
+                .createDate(reservation.getCreateDate())
+                .createTime(reservation.getCreateTime())
+                .personnel(reservation.getPersonnel())
+                .addOptions(reservation.getReservationProductAddOptions() != null ?
+                        reservation.getReservationProductAddOptions().stream()
+                                .map(ReservationProductAddOption::getId)
+                                .collect(Collectors.toList()) : Collections.emptyList())
+                .build();
+    }
 }
