805c171b0dd41de2370bceca236bc8fdf7cddf04
m1nddoong <alstjsdlr0321@korea.ac.kr>
2025-02-07 20:57:00 +0900

    feat: Swagger 헤더 설정 자물쇠 추가

--- src/main/java/com/toucheese/global/config/SwaggerConfig.java
+++ src/main/java/com/toucheese/global/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.toucheese.global.config;
 
 import java.util.List;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,21 +16,35 @@ import io.swagger.v3.oas.models.servers.Server;
 @Configuration
 public class SwaggerConfig {
 
-	@Value("${springdoc.request-url}")
-	private String requestUrl;
+    @Value("${springdoc.request-url}")
+    private String requestUrl;
 
-	@Bean
-	public OpenAPI openAPI() {
-		Info info = new Info()
-			.version("v1")
-			.title("Open API Definition");
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .version("v1")
+                .title("Open API Definition");
 
 
-		Server server = new Server();
-		server.setUrl(requestUrl);
+        Server server = new Server();
+        server.setUrl(requestUrl);
 
-		return new OpenAPI()
-			.info(info)
-			.servers(List.of(server));
-	}
+        String jwtSchemeName = "jwtAuth";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(
+                        jwtSchemeName,
+                        new SecurityScheme()
+                                .name(jwtSchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                );
+
+        return new OpenAPI()
+                .info(info)
+                .components(components)
+                .addSecurityItem(securityRequirement)
+                .servers(List.of(server));
+    }
 }
