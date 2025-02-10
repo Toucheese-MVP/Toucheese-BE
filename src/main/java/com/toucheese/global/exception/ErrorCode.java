b35804a7e004cb5509bd8eabefef3fc75ddcbab3
m1nddoong <alstjsdlr0321@korea.ac.kr>
2025-02-10 14:35:27 +0900

    feat: kakao 회원탈퇴 기능 추가

--- src/main/java/com/toucheese/global/exception/ErrorCode.java
+++ src/main/java/com/toucheese/global/exception/ErrorCode.java
@@ -25,7 +25,7 @@ public enum ErrorCode {
     APPLE_AUTH_TOKEN_FAIL(4013, HttpStatus.INTERNAL_SERVER_ERROR, "애플 인증 토큰 생성에 실패하였습니다."),
     FAIL_TO_LOAD_PRIVATE_KEY(4014, HttpStatus.INTERNAL_SERVER_ERROR, "Apple private 키를 가져오는데 실패하였습니다."),
     INVALID_APPLE_ACCESS_TOKEN(4015, HttpStatus.BAD_REQUEST, "애플 액세스 토큰이 유효하지 않습니다."),
-    APPLE_REVOKE_TOKEN_FAIL(4016, HttpStatus.BAD_REQUEST, "애플 액세스 토큰 무효화에 실패하였습니다."),
+    APPLE_REVOKE_TOKEN_FAIL(4016, HttpStatus.INTERNAL_SERVER_ERROR, "애플 액세스 토큰 무효화에 실패하였습니다."),
     LOGOUT_UNAUTHORIZED_ACCESS(4017, HttpStatus.UNAUTHORIZED, "로그아웃에 실패하였습니다.")      
     ;
 
