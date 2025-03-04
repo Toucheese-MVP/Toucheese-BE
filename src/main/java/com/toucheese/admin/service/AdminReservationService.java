44770184f34fe0c87f04f01caeeaae83f1e08bff
JeUk Park <77144006+sleo113@users.noreply.github.com>
2025-03-04 13:30:43 +0900

    feat: 예약 접수 확정 메시지 보내기

--- src/main/java/com/toucheese/admin/service/AdminReservationService.java
+++ src/main/java/com/toucheese/admin/service/AdminReservationService.java
@@ -1,7 +1,15 @@
 package com.toucheese.admin.service;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
+import com.toucheese.global.exception.ErrorCode;
+import com.toucheese.global.exception.GlobalCustomException;
+import com.toucheese.member.entity.Member;
+import com.toucheese.member.repository.MemberRepository;
+import com.toucheese.reservation.repository.ReservationRepository;
+import com.toucheese.solapi.util.SolapiUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -14,12 +22,16 @@ import com.toucheese.reservation.entity.ReservationStatus;
 import com.toucheese.reservation.service.ReservationReadService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.StringUtils;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AdminReservationService {
 
 	private final ReservationReadService reservationReadService;
+	private final ReservationRepository reservationRepository;
+	private final SolapiUtil solapiUtil;
 
 	@Transactional(readOnly = true)
 	public Page<AdminReservationListResponse> findReservations(ReservationStatus status, LocalDate createDate,
@@ -32,8 +44,22 @@ public class AdminReservationService {
 
 	@Transactional
 	public void updateReservationStatus(Long reservationId, ReservationStatus newStatus) {
+		// 예약 정보를 조회
 		Reservation reservation = reservationReadService.findReservationById(reservationId);
 
-		reservation.updateStatus(newStatus);
+		// 예약이 존재하지 않을 경우 예외 처리
+		if(reservation == null ){
+			throw new GlobalCustomException(ErrorCode.RESERVATION_NOT_FOUND);
+		}
+
+		// 예약 상태를 "예약 확정"으로 변경
+		if(newStatus == ReservationStatus.예약확정){
+			String messageText = solapiUtil.determineFormatMessage(reservation.getMember().getName());
+			String registeredSenderNumber = "01098455844";
+			solapiUtil.send(registeredSenderNumber, reservation.getMember().getPhone(), messageText);
+		}
+
+		reservation.updateStatus(newStatus); // 예약 상태 업데이트
+		reservationRepository.save(reservation); // 변경 사항 저장
 	}
 }
