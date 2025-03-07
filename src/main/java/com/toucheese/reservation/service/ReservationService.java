f6d59f4358cdabe19a88099636b50b61452f8f57
JeUk Park <77144006+sleo113@users.noreply.github.com>
2025-03-07 14:50:04 +0900

    feat: 찜하기 엔티티 작성

--- src/main/java/com/toucheese/reservation/service/ReservationService.java
+++ src/main/java/com/toucheese/reservation/service/ReservationService.java
@@ -1,13 +1,17 @@
 package com.toucheese.reservation.service;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.toucheese.global.exception.ErrorCode;
 import com.toucheese.global.exception.GlobalCustomException;
+import com.toucheese.global.fcm.FcmService;
+import com.toucheese.global.fcm.PushMsg;
 import com.toucheese.member.entity.Member;
 import com.toucheese.member.repository.MemberRepository;
 import com.toucheese.member.service.MemberService;
@@ -15,6 +19,8 @@ import com.toucheese.reservation.dto.ReservationRequest;
 import com.toucheese.reservation.dto.ReservationSuccessResponse;
 import com.toucheese.solapi.util.SolapiUtil;
 import com.toucheese.studio.service.StudioService;
+import jakarta.annotation.PostConstruct;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -44,6 +50,7 @@ public class ReservationService {
 	private final ProductService productService;
 	private final MemberRepository memberRepository;
 	private final SolapiUtil solapiUtil;
+	private final FcmService fcmService;
 
 	@Transactional
 	public void createReservationsFromCarts(List<Cart> carts) {
@@ -108,7 +115,6 @@ public class ReservationService {
 		// 회원이 존재하지 않을 경우 예외 처리
 		if (memberOpt.isEmpty()) {
 			throw new GlobalCustomException(ErrorCode.MEMBER_NOT_FOUND);
-
 		}
 
 		// 회원의 전화번호 유무 확인
@@ -127,7 +133,6 @@ public class ReservationService {
 				throw new GlobalCustomException(ErrorCode.PHONE_REQUEST_NOT_FOUND);
 			}
 		}
-
 
 		// 새로운 예약 생성
 		Reservation reservation = Reservation.builder()
@@ -154,6 +159,35 @@ public class ReservationService {
 				.status(true)
 				.build();
 	}
+
+	/*
+	 * 알림 로직
+	 * memberId - 푸시 메시지 받아야 하는 멤버 아이디
+	 * reservation - studio 이름을 얻기 위한 것이므로 Studio, studioName 등으로 수정 가능
+	 * pushMsg - 메시지 종류 PushMsg ENUM 참고 -> 예약 성공: PushMsg.RESERVATION_SUCCEED, 예약 실패: PushMsg.RESERVATION_FAILED
+	 */
+	// 푸시 알림을 보내는 메서드
+//	public void sendPushMsg(Long memberId, Reservation reservation, PushMsg pushMsg) {
+		// 해당 사용자 디바이스 토큰을 Redis에서 가져옵니다
+//		String deviceToken = deviceService.getDeviceToken(memberId);
+//
+//		// 만약 토큰이 존재하면 푸시 메시지를 전송합니다
+//		if (deviceToken != null) {
+//			try {
+//				fcmService.sendPushMsg(
+//						deviceToken,
+//						pushMsg,
+//						reservation.getStudio().getName()
+//				);
+//			} catch (IOException | FirebaseMessagingException e) {
+//				throw new RuntimeException(e);
+//			}
+//		} else {
+//			// 토큰이 없으면 알림을 보내지 않음
+//			// 필요한 경우 로깅 또는 다른 처리를 할 수 있습니다.
+//			log.info("Device token not found, push notification not sent.");
+//		}
+//	}
 
 
 	//		// 회원의 전화번호 유무 확인
