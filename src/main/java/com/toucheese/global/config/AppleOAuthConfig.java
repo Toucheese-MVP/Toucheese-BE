72b2f712501cfda1df23ab2352caf6cf8b31eb54
m1nddoong <alstjsdlr0321@korea.ac.kr>
2025-03-10 17:14:05 +0900

    feat: Feign Client 타임아웃 설정 및 예외처리

--- src/main/java/com/toucheese/global/config/AppleOAuthConfig.java
+++ src/main/java/com/toucheese/global/config/AppleOAuthConfig.java
@@ -3,6 +3,7 @@ package com.toucheese.global.config;
 
 
 import com.toucheese.global.exception.FeignErrorDecoder;
+import feign.Request;
 import feign.Retryer;
 import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppleOAuthConfig {
+    @Bean
+    public Request.Options options() {
+
+        return new Request.Options(5000, 30000);// 커넥션 타임아웃 5초, 읽기 타임아웃 30초
+    }
+
     @Bean
     Retryer.Default retryer() {
         return new Retryer.Default(1000, 1500, 1);
@@ -20,6 +27,4 @@ public class AppleOAuthConfig {
         return new FeignErrorDecoder();
     }
 }
-
-
 
