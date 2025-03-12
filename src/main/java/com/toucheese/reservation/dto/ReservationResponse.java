594d47b73ea6e47fea928ed69f1ac0cabc1b6178
m1nddoong <alstjsdlr0321@korea.ac.kr>
2025-03-12 13:34:16 +0900

    feat: 에약 확인 productId 필드 추가

--- src/main/java/com/toucheese/reservation/dto/ReservationResponse.java
+++ src/main/java/com/toucheese/reservation/dto/ReservationResponse.java
@@ -11,28 +11,30 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record ReservationResponse (
-	Long reservationId,
-	Long studioId,
-	String studioName,
-	String studioImage,
-	String productName,
-	LocalDate createDate,
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
-	@Schema(type = "string", example = "19:00")
-	LocalTime createTime,
-	ReservationStatus status
-){
-	public static ReservationResponse of(Reservation reservation, String baseUrl) {
-		return builder()
-			.reservationId(reservation.getId())
-			.studioId(reservation.getStudio().getId())
-			.studioName(reservation.getStudio().getName())
-			.studioImage(baseUrl + reservation.getStudio().getProfileImage())
-			.productName(reservation.getProduct().getName())
-			.createDate(reservation.getCreateDate())
-			.createTime(reservation.getCreateTime())
-			.status(reservation.getStatus())
-			.build();
-	}
+public record ReservationResponse(
+        Long reservationId,
+        Long studioId,
+        String studioName,
+        String studioImage,
+        Long productId,
+        String productName,
+        LocalDate createDate,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+        @Schema(type = "string", example = "19:00")
+        LocalTime createTime,
+        ReservationStatus status
+) {
+    public static ReservationResponse of(Reservation reservation, String baseUrl) {
+        return builder()
+                .reservationId(reservation.getId())
+                .studioId(reservation.getStudio().getId())
+                .studioName(reservation.getStudio().getName())
+                .studioImage(baseUrl + reservation.getStudio().getProfileImage())
+                .productId(reservation.getProduct().getId())
+                .productName(reservation.getProduct().getName())
+                .createDate(reservation.getCreateDate())
+                .createTime(reservation.getCreateTime())
+                .status(reservation.getStatus())
+                .build();
+    }
 }
