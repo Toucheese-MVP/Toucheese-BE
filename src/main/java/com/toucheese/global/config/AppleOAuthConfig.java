79c9c4f5462e7cb6cdf79e4ba4c58a91c1fa4d68
m1nddoong <alstjsdlr0321@korea.ac.kr>
2025-03-12 15:08:31 +0900

    feat: github actions workflow 수정

--- src/main/java/com/toucheese/global/config/AppleOAuthConfig.java
+++ src/main/java/com/toucheese/global/config/AppleOAuthConfig.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
 public class AppleOAuthConfig {
     @Bean
     public Request.Options options() {
-
         return new Request.Options(5000, 30000);// 커넥션 타임아웃 5초, 읽기 타임아웃 30초
     }
 
