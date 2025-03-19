ee10319ce473d9db4d3ee96af077f4e9fc5d343c
m1nddoong <alstjsdlr0321@korea.ac.kr>
2025-03-19 14:07:05 +0900

    refactor: fcmConfig 수정

--- src/main/java/com/toucheese/firebase/config/FcmConfig.java
+++ src/main/java/com/toucheese/firebase/config/FcmConfig.java
@@ -4,52 +4,46 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.toucheese.firebase.controller.FirebaseApi;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
-@Slf4j
 public class FcmConfig {
-    @Value("${firebase.config.path}")
-    String firebaseConfigPath;
 
+    private final ClassPathResource firebaseResource;
 
-    @Bean
-    public FirebaseApp firebaseApp() {
-        try {
-            File file = new File(firebaseConfigPath);
-            log.info("생성한 File 객체 경로 확인 : " + file.getAbsolutePath());
-            if (!file.exists()) {
-                throw new IOException("File 객체가 존재하지 않음");
-            }
+    public FcmConfig(
+            @Value("${fcm.file_path}")
+            ClassPathResource firebaseResource
+    ) {
+        this.firebaseResource = firebaseResource;
+    }
 
-            FileInputStream serviceAccount =
-                    new FileInputStream(firebaseConfigPath);
-
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
-            // FirebaseApp이 이미 초기화되었는지 확인
-            if (FirebaseApp.getApps().isEmpty()) {
-                return FirebaseApp.initializeApp(options);
-            } else {
-                return FirebaseApp.getInstance();
-            }
-        } catch (IOException e) {
-            log.error("Firebase 초기화 과정 중 문제 발생: " + e.getMessage(), e);
-            throw new RuntimeException(e);
+    @PostConstruct
+    public void init() throws IOException {
+        val option = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(firebaseResource.getInputStream()))
+                .build();
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(option);
         }
     }
 
     @Bean
-    public FirebaseMessaging firebaseMessaging() {
+    FirebaseMessaging firebaseMessaging() {
         return FirebaseMessaging.getInstance(firebaseApp());
+    }
+
+    @Bean
+    FirebaseApp firebaseApp() {
+        return FirebaseApp.getInstance();
     }
 }
