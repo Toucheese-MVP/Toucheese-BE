0bc4dc7a45b9daf8aa910ccccbea2f76e5349167
youngwonkim <bismute@gmail.com>
2025-06-09 10:28:16 +0900

    feat: 장바구니 상품등록시 selectAddOptions 관련 수정.

--- src/main/java/com/toucheese/cart/dto/CheckoutCartItemsResponse.java
+++ src/main/java/com/toucheese/cart/dto/CheckoutCartItemsResponse.java
@@ -9,17 +9,35 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record CheckoutCartItemsResponse (
+	@Schema(description = "장바구니 ID", example = "1")
 	Long cartId,
+	@Schema(description = "스튜디오 이름", example = "포토스튜디오")
 	String studioName,          // 스튜디오 이름
+	@Schema(description = "상품 이미지 URL", example = "https://example.com/image.jpg")
 	String productImage,        // 상품이미지
+	@Schema(description = "상품 이름", example = "기본 패키지")
 	String productName,         // 상품 이름
+	@Schema(description = "상품 가격", example = "100000")
 	Integer productPrice,		// 상품 가격
+	@Schema(description = "예약 인원", example = "2")
 	Integer personnel,          // 예약 인원
+	@Schema(description = "예약 날짜", example = "2024-03-21")
 	LocalDate reservationDate,  // 예약 날짜
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
-	@Schema(type = "string", example = "19:00")
+	@Schema(type = "string", description = "예약 시간", example = "19:00")
 	LocalTime reservationTime,  // 예약 시간
+	@Schema(description = "상품 1개 전체 가격 (상품 가격 + 선택한 옵션 가격)", example = "150000")
 	Integer totalPrice,         // 상품 1개 전체 가격
+	@Schema(description = "선택한 추가 옵션 정보", example = """
+		[
+			{
+				"selectOptionId": 1,
+				"selectOptionName": "헤어",
+				"selectOptionPrice": 10000,
+				"addOptPerPerson": 2
+			}
+		]
+		""")
 	List<SelectAddOptionResponse> selectAddOptions // 선택한 추가 옵션 정보
 ) {
 }
--- src/main/java/com/toucheese/cart/dto/SelectAddOptionResponse.java
+++ src/main/java/com/toucheese/cart/dto/SelectAddOptionResponse.java
@@ -7,8 +7,11 @@ import lombok.Builder;
 
 @Builder
 public record SelectAddOptionResponse(
-	Long selectOptionId,             //
+	@Schema(description = "선택한 옵션의 ID", example = "1")
+	Long selectOptionId,             // 옵션 ID
+	@Schema(description = "선택한 옵션의 이름", example = "헤어")
 	String selectOptionName,       // 옵션 이름
+	@Schema(description = "선택한 옵션의 가격", example = "10000")
 	Integer selectOptionPrice,     // 옵션 가격
 	@Schema(description = "인원당 추가 옵션 개수 (standard가 2인 상품의 경우)", example = "2")
 	Integer addOptPerPerson
